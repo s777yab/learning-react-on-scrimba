30e769f201d00785cdd3b6fa64012d2011cd62a0
Sayyab Khan <sayyabk05@gmail.com>
2024-03-19 21:14:29 +0000

    Learning composability in react

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ the line of code you just learned! Nope, not on my watch!
 
 Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+
+function MainContent() {
+  return (
+    <h1>I am learning composability in React!</h1>
+  )
+}
+
 ReactDOM.render(
     <ul><li>Thing 1</li><li>Thing 2</li></ul>,
     document.getElementById("root")
+)
+
+ReactDOM.render(
+  <div>
+    <MainContent />
+  </div>,
+  document.getElementById("root")
 )
